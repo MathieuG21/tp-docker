8fd888c1334f5dc5a128d297048f2fd3265adea9
MathieuGAY21 <m.gay@standardenligne.fr>
2024-11-07 10:44:48 +0100

    first commit

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Salade tomate oignon!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
+});
